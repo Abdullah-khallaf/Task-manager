3ac10114b851ce7b0469092b1718ef0e7c8c184b
Abdullah-khallaf <abdullah.khallaf.work@gmail.com>
2023-07-20 10:31:24 +0300

    Commit -1: basic structure for vue project

---
+++ Task-Manager/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>
+      This App Is Underconstruction. 
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+  h1{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    font-family: 'Poppins', sans-serif;
+    font-size: 48px;
+    font-weight: bold;
+    }
+</style>
