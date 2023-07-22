68d9256c99591ba37eab2f7608aab88947ff5e19
iamhashemtarek <hashemtarek2012@gmail.com>
2023-07-22 21:03:20 +0300

    handling unhandled rejections and exceptions

--- Task-Manager/src/server/api/resources/user/user.service.js
+++ Task-Manager/src/server/api/resources/user/user.service.js
@@ -1,13 +1,13 @@
-import catchAsync from '../../../utils/catchAsync.js';
-import connect from '../../database/index.js'
+import catchAsync from "../../../utils/catchAsync.js";
+import connect from "../../database/index.js";
+import AppError from "../../../utils/appError.js";
 
 export const getAllUsers = async () => {
-    const db = await connect();
+  const db = await connect();
 
-    const sql = `
-        select * from users
-    `;
-    const [[rows]] = await db.query(sql);
-    return rows;
-
-}
+  const sql = `
+    select * from users
+`;
+  const [rows] = await db.query(sql);
+  return rows;
+};
--- Task-Manager/src/server/server.js
+++ Task-Manager/src/server/server.js
@@ -1,3 +1,10 @@
+// handling uncaught exception
+process.on('uncaughtException', (err) => {
+    console.log(`name: ${err.name}, message: ${err.message}`);
+    console.log(('uncaughtException, shutting down'));
+    process.exit(1);
+})
+
 import express from "express";
 import config from './config/config.js'
 import morgan from "morgan";
@@ -16,7 +23,20 @@ app.use(globalErrorHandler)
 //routes
 app.use('/api/v1/user', userRoutes)
 
+process.on('unhandledRejection', () => {
+
+})
+
 const PORT =  config.PORT || 8080 
 app.listen(8080,() => {
     console.log(`app is listening on port ${PORT}`);
+})
+
+// handling unhandled Rejections
+process.on('unhandledRejection', (err) => {
+    console.log(`name: ${err.name}, message: ${err.message}`);
+    console.log(('unhandledRejection, shutting down'));
+    server.close(() => {
+        process.exit(1);
+    })
 })
