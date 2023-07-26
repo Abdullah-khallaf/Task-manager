8c5044cd03c841ba62f65504a8b3a94764ddae93
iamhashemtarek <hashemtarek2012@gmail.com>
2023-07-26 11:04:24 +0300

    configure db for jest

--- Task-Manager/src/server/api/database/index.js
+++ Task-Manager/src/server/api/database/index.js
@@ -7,18 +7,24 @@ export default () => {
     return pool;
   }
 
-  pool = mysql
-    .createPool({
-      host: config.DB_HOST,
-      port: config.DB_PORT,
-      user: config.DB_USER,
-      password: config.DB_PASSWORD,
-      database: config.DB_NAME,
-    })
-    .promise();
+  let db_name;
+  if (config.NODE_ENV == "dev") {
+    db_name = config.DB_NAME;
+  } else if (config.NODE_ENV == "test") {
+    db_name = config.TEST_DB_NAME;
+  }
+
+  const poolConfig = {
+    host: config.DB_HOST,
+    port: config.DB_PORT,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    database: db_name,
+  };
+  pool = mysql.createPool(poolConfig).promise();
 
   pool.on("connection", () => {
-    console.log("connected to mysql");
+    console.log(`connected to db: ${db_name}`);
   });
   pool.on("release", () => {
     console.log("mysql connection is released");
