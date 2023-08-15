c343aa6421881052a5ac11ca1b259b0d50d65a53
Abdullah-khallaf <abdullah.khallaf.work@gmail.com>
2023-08-15 19:47:09 +0300

    side bar menue

---
+++ Task-Manager/src/components/SideBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <!--<div class="sidebar">
+        <div class="logo">
+        <img src="../assets/logo.png" alt="logo" />
+        </div>
+        <div class="menu">
+        <ul>
+            <li>
+            <router-link to="/">Home</router-link>
+            </li>
+            <li>
+            <router-link to="/login">Login</router-link>
+            </li>
+            <li>
+            <router-link to="/signup">Signup</router-link>
+            </li>
+        </ul>
+        </div>
+    </div>-->
+    
+    <div>
+        <p>
+            number Of Words = {{ numberOfWords }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default{
+        computed:{
+            numberOfWords(){
+                return 10
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
