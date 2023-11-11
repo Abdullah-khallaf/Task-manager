0e60f1aad24d9c55170f26459b743243bdc1b912
iamhashemtarek <hashemtarek2012@gmail.com>
2023-11-11 22:45:39 +0200

    update cors configurations

--- Task-Manager/src/server/app.js
+++ Task-Manager/src/server/app.js
@@ -34,7 +34,12 @@ if (config.env === "prod") {
 }
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(
   session({
