a3a78af6d5b122e904725ebd077eb441b2c1cbf1
Abdullah-khallaf <abdullah.khallaf.work@gmail.com>
2024-01-28 16:28:18 +0200

    task Input component, it handles the task object.

---
+++ Task-Manager/src/components/formsComponents/taskForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form>
+    <input type="text" id="taskInput" v-model="newTask" placeholder="Add Task" @keyup.enter="addTask">
+    <button @click="addTask()"> Add Task</button>
+  </form> 
+</template>
+
+<script>
+
+
+export default{
+  data(){
+    return{
+      newTask : '',
+      
+    }
+  },
+  methods: {
+    addTask(){
+      event.preventDefault(); // to prevent any dafault behavior
+      if(!this.newTask.trim()){  // we add this code to take only value and cancel the white input
+        return;
+      } 
+      const newTaskOBJ = {
+        id: Date.now(), // it is common to use the date of the new input as an Identifier.
+        title: this.newTask, // the title of the new task is the value of the input
+        isImportant: false,
+        isChecked: false,
+      };
+    
+      this.$emit('task-added', newTaskOBJ); //event to connect the task form with task view
+      console.log("Task Added");
+
+      this.newTask = ''; // to clear the input after adding the task
+    }
+  }
+}
+ </script>
+
+ <style scoped>
+ 
+</style>
+
